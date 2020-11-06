76448e18bd4d6aedf46c365ecb9a49ac3cd56863
Angel Flores <aflores@zondahome.com>
2020-11-06 14:26:06 -0800

    Origin changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { map, fn } from 'terraform-generator';
 import * as path from 'path';
 import { tfg } from './tfg';
 
@@ -11,13 +10,17 @@ const s3TestBucket = tfg.resource('aws_s3_bucket', 's3_test_bucket', {
 });
 
 const originId = 'testS3Origin';
+const rootOriginId = `S3-${s3TestBucket.attr('bucket')}/root`
 
 tfg.resource('aws_cloudfront_distribution', 'frontend_distribution', {
-  origin: {
+  origin: [{
     domain_name: s3TestBucket.attr('bucket_regional_domain_name'),
     origin_id: originId,
     origin_path: '/envs/dev'
-  },
+  }, {
+    domain_name: s3TestBucket.attr('bucket_regional_domain_name'),
+    origin_id: rootOriginId
+  }],
 
   enabled: true,
   is_ipv6_enabled: true,
@@ -38,7 +41,7 @@ tfg.resource('aws_cloudfront_distribution', 'frontend_distribution', {
     target_origin_id: originId,
 
     forwarded_values: {
-      query_string: false,
+      query_string: true,
       cookies: {
         forward: "none"
       }
@@ -48,6 +51,25 @@ tfg.resource('aws_cloudfront_distribution', 'frontend_distribution', {
     min_ttl: 0,
     default_ttl: 86400,
     max_ttl: 31536000
+  },
+
+  ordered_cache_behavior: {
+    path_pattern: '/build/*',
+    allowed_methods: ['GET', 'HEAD'],
+    cached_methods: ["GET", "HEAD"],
+    target_origin_id: rootOriginId,
+
+    forwarded_values: {
+      query_string: false,
+      cookies: {
+        forward: "none"
+      }
+    },
+
+    viewer_protocol_policy: 'redirect-to-https',
+    min_ttl: 0,
+    default_ttl: 86400,
+    max_ttl: 31536000 
   },
 
   custom_error_response: [{
